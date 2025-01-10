7c305af51d1cb4f0dcd7595e56666c1d424a317c
Teddy Aboim <teddy.aboim@gmail.com>
2025-01-10 19:02:14 +0100

    add first routes

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-console.log("hello world");
+const express = require("express");
+const { success } = require("./helper");
+let pokemons = require("./mock-pokemon");
+
+const app = express();
+const port = 8000;
+
+// Routes
+app.get("/", (req, res) => res.send("Hello express !"));
+
+app.get("/api/pokemons", (req, res) => {
+    const message = "La liste des pokémons a bien été récupérée.";
+    res.json(success(message, pokemons));
+});
+
+app.get("/api/pokemons/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+    const message = "Un pokémon a bien été trouvé.";
+    res.json(success(message, pokemon));
+});
+
+// Server
+app.listen(port, () =>
+    console.log(`Application started on http://localhost:${port}`)
+);
