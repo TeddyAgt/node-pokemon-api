a1efb8a5832b0b5ad99f478b6e3312f8f2548ec8
Teddy Aboim <teddy.aboim@gmail.com>
2025-01-11 17:02:50 +0100

    add data control and error managment

--- src/Models/pokemon.js
+++ src/Models/pokemon.js
@@ -1,3 +1,15 @@
+const validTypes = [
+    "Plante",
+    "Poison",
+    "Feu",
+    "Eau",
+    "Insecte",
+    "Vol",
+    "Normal",
+    "Electrik",
+    "Fée",
+];
+
 module.exports = (sequelize, DataTypes) => {
     return sequelize.define(
         "Pokemon",
@@ -10,18 +22,59 @@ module.exports = (sequelize, DataTypes) => {
             name: {
                 type: DataTypes.STRING,
                 allowNull: false,
+                unique: {
+                    msg: "Le nom est déjà pris",
+                },
+                validate: {
+                    notEmpty: { msg: "Le nom ne peut pas être vide" },
+                    notNull: { msg: "Le nom ne peut pas être vide" },
+                },
             },
             hp: {
                 type: DataTypes.INTEGER,
                 allowNull: false,
+                validate: {
+                    isInt: { msg: "Utilisez uniquement des nombres" },
+                    notNull: {
+                        msg: "Les points de vie ne peuvent pas être nuls",
+                    },
+                    min: {
+                        args: [0],
+                        msg: "Les points de vie doivent être au minimum à 0",
+                    },
+                    max: {
+                        args: [999],
+                        msg: "Les points de vie ont un maximum de 999",
+                    },
+                },
             },
             cp: {
                 type: DataTypes.INTEGER,
                 allowNull: false,
+                validate: {
+                    isInt: { msg: "Utilisez uniquement des nombres" },
+                    notNull: {
+                        msg: "Les points de combat ne peuvent pas être nuls",
+                    },
+                    min: {
+                        args: [0],
+                        msg: "Les points de combat doivent être au minimum à 0",
+                    },
+                    max: {
+                        args: [99],
+                        msg: "Les points de combat ont un maximum de 99",
+                    },
+                },
             },
             picture: {
                 type: DataTypes.STRING,
                 allowNull: false,
+                validate: {
+                    isUrl: { msg: "Utilisez une URL valide" },
+                    notNull: {
+                        msg: "L'image est requise",
+                    },
+                },
             },
             types: {
                 type: DataTypes.STRING,
@@ -31,6 +84,27 @@ module.exports = (sequelize, DataTypes) => {
                 },
                 set(types) {
                     this.setDataValue("types", types.join());
+                },
+                validate: {
+                    isTypeValid(value) {
+                        if (!value) {
+                            throw new Error(
+                                "Un pokémon doit avoir au moins un type"
+                            );
+                        }
+                        if (value.split(",").length > 3) {
+                            throw new Error(
+                                "Un pokémon doit avoir au maximum 3 types"
+                            );
+                        }
+                        value.split(",").forEach((type) => {
+                            if (!validTypes.includes(type)) {
+                                throw new Error(
+                                    `Le type "${type}  n'est pas valide. Liste des types: ${validTypes}`
+                                );
+                            }
+                        });
+                    },
                 },
             },
         },
--- src/Routes/updatePokemon.js
+++ src/Routes/updatePokemon.js
@@ -1,3 +1,4 @@
+const { ValidationError } = require("sequelize");
 const { Pokemon } = require("../db/sequelize");
 
 module.exports = (app) => {
@@ -13,11 +14,23 @@ module.exports = (app) => {
                             "Le pokémon demandé n'éxiste pas. Réessayez avec un autre identifiant.";
                         return res.status(404).json({ message });
                     }
+                    console.log("ici");
                     const message = `Le pokémon ${pokemon.name} a bien été modifié.`;
                     res.json({ message, data: pokemon });
                 });
             })
             .catch((error) => {
+                if (error instanceof ValidationError) {
+                    return res
+                        .status(400)
+                        .json({ message: error.message, data: error });
+                }
+                if (error instanceof UniqueConstraintError) {
+                    return res
+                        .status(400)
+                        .json({ message: error.message, data: error });
+                }
+
                 const message =
                     "Le pokémon n'a pas pu être modifié. Réessayez dans quelques instants";
                 res.status(500).json({ messsage, data: error });
