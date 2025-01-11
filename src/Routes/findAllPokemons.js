4d9d5aac337a65296e5c96e143a8f4da78101283
Teddy Aboim <teddy.aboim@gmail.com>
2025-01-11 17:26:20 +0100

    add search feature

--- src/Routes/findAllPokemons.js
+++ src/Routes/findAllPokemons.js
@@ -1,15 +1,33 @@
 const { Pokemon } = require("../db/sequelize");
+const { Op } = require("sequelize");
 
 module.exports = (app) => {
     app.get("/api/pokemons", (req, res) => {
-        Pokemon.findAll()
-            .then((pokemons) => {
-                const message = "La liste des pokémons a bien été récupérée.";
-                res.json({ message, data: pokemons });
+        if (req.query.name) {
+            const name = req.query.name;
+            return Pokemon.findAll({
+                where: {
+                    name: {
+                        [Op.like]: `%${name}%`,
+                    },
+                },
             })
-            .catch((error) => {
-                const message = `La liste des pokémons n'a pas pu être récupérée. Réessayez dans quelques instants.`;
-                res.status(500).json({ message, data: error });
-            });
+                .then((pokemons) => {
+                    const message = `Il y a ${pokemons.length} pokémons qui correspondent à la recherche '${name}'`;
+                    res.json({ message, data: pokemons });
+                })
+                .catch();
+        } else {
+            Pokemon.findAll()
+                .then((pokemons) => {
+                    const message =
+                        "La liste des pokémons a bien été récupérée.";
+                    res.json({ message, data: pokemons });
+                })
+                .catch((error) => {
+                    const message = `La liste des pokémons n'a pas pu être récupérée. Réessayez dans quelques instants.`;
+                    res.status(500).json({ message, data: error });
+                });
+        }
     });
 };
